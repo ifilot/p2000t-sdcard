1261ab8431d152e50b531c1d0297903aacdabea0
ifilot <ivo@ivofilot.nl>
2024-03-17 21:45:27 +0100

    Adding XOR a before RUN

--- basicmod/bootstrap.asm
+++ basicmod/bootstrap.asm
@@ -103,6 +103,7 @@ loadrom:
     call read_ram       ; load low byte file size
     ld c,a
     call copydata       ; bc contains number of bytes
+    xor a               ; set flags z, nc
     jp $28d4            ; launch basic program
 
 msglp:
--- src/config.h
+++ src/config.h
@@ -1,6 +1,6 @@
 #ifndef _CONFIG_H
 #define _CONFIG_H
 
-#define __VERSION__ "0.1.1"
+#define __VERSION__ "0.1.2"
 
 #endif
