3b287f7efc5d41856f074a53731cbf7b0a7ab6f1
ifilot <ivo@ivofilot.nl>
2024-05-02 07:48:24 +0200

    Adding comments

--- src/ascii.h
+++ src/ascii.h
@@ -1,3 +1,23 @@
+/**************************************************************************
+ *                                                                        *
+ *   Author: Ivo Filot <ivo@ivofilot.nl>                                  *
+ *                                                                        *
+ *   P2000T-SDCARD is free software:                                      *
+ *   you can redistribute it and/or modify it under the terms of the      *
+ *   GNU General Public License as published by the Free Software         *
+ *   Foundation, either version 3 of the License, or (at your option)     *
+ *   any later version.                                                   *
+ *                                                                        *
+ *   P2000T-SDCARD is distributed in the hope that it will be useful,     *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty          *
+ *   of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.              *
+ *   See the GNU General Public License for more details.                 *
+ *                                                                        *
+ *   You should have received a copy of the GNU General Public License    *
+ *   along with this program.  If not, see http://www.gnu.org/licenses/.  *
+ *                                                                        *
+ **************************************************************************/
+
 uint8_t __ascii[] = {
     0x10,0x36,0x11,0x71,0x33,0x35,0x37,0x34,
     0x09,0x68,0x7A,0x73,0x64,0x67,0x6A,0x66,
